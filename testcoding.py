0361fb2597d6f1fb0eb4130cb4ee59620f744736
Carlo Goetz <carlo.goetz@googlemail.com>
2017-09-30 19:53:47 +0200

    Merge branch 'master' of https://github.com/AdrianLoer/Hue_hackerthon
    # Conflicts:
    #	testcoding.py

--- testcoding.py
+++ testcoding.py
@@ -10,6 +10,41 @@ import datetime
 tick = datetime.timedelta(milliseconds=500)
 
 scenes = {"000": 0, "001": 1, "010": 2, "011": 3, "100": 4, "101": 5, "110": 6, "111": 7}
+groupid = 1
+
+def init_scenes(bridge):
+    bridge.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': False, 'transitiontime': 0})
+    bridge.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": False, 'transitiontime': 0})
+    bridge.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': False, 'transitiontime': 0})
+    bridge.request(mode='POST', address='/api/' + bridge.username + '/scenes/', data={'name': '000', 'lights': ['1', '2', '3'], 'recycle': True})
+    bridge.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': False, 'transitiontime': 0})
+    bridge.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": False, 'transitiontime': 0})
+    bridge.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
+    bridge.request(mode='POST', address='/api/' + bridge.username + '/scenes/', data={'name': '001', 'lights': ['1', '2', '3'], 'recycle': True})
+    bridge.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': False, 'transitiontime': 0})
+    bridge.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": True, 'transitiontime': 0})
+    bridge.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': False, 'transitiontime': 0})
+    bridge.request(mode='POST', address='/api/' + bridge.username + '/scenes/', data={'name': '010', 'lights': ['1', '2', '3'], 'recycle': True})
+    bridge.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': False, 'transitiontime': 0})
+    bridge.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": True, 'transitiontime': 0})
+    bridge.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
+    bridge.request(mode='POST', address='/api/' + bridge.username + '/scenes/', data={'name': '011', 'lights': ['1', '2', '3'], 'recycle': True})
+    bridge.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
+    bridge.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": False, 'transitiontime': 0})
+    bridge.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': False, 'transitiontime': 0})
+    bridge.request(mode='POST', address='/api/' + bridge.username + '/scenes/', data={'name': '100', 'lights': ['1', '2', '3'], 'recycle': True})
+    bridge.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
+    bridge.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": False, 'transitiontime': 0})
+    bridge.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
+    bridge.request(mode='POST', address='/api/' + bridge.username + '/scenes/', data={'name': '101', 'lights': ['1', '2', '3'], 'recycle': True})
+    bridge.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
+    bridge.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": True, 'transitiontime': 0})
+    bridge.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': False, 'transitiontime': 0})
+    bridge.request(mode='POST', address='/api/' + bridge.username + '/scenes/', data={'name': '110', 'lights': ['1', '2', '3'], 'recycle': True})
+    bridge.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
+    bridge.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": True, 'transitiontime': 0})
+    bridge.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
+    bridge.request(mode='POST', address='/api/' + bridge.username + '/scenes/', data={'name': '111', 'lights': ['1', '2', '3'], 'recycle': True})
 
 
 def say_nrz(bridge, data, lights):
@@ -19,53 +54,18 @@ def say_nrz(bridge, data, lights):
     stuff = [0] * stuffing
     data = stuff + data
     length += stuffing
+    for scene in bridge.scenes:
+        if scene.name in scenes.keys():
+            scenes[scene.name] = scene.scene_id
     while sent < length:
         start = datetime.datetime.now()
-
-        # for i in range(0, len(lights)):
-        #    bridge.set_light(lights[i], {'on': not not data[sent], 'transitiontime': 0, 'bri': 255})
         scene_id = scenes[(''.join([str(x) for x in data[sent:sent + 3]]))]
-        bridge.run_scene("hueheffner", scene_id)
+        bridge.activate_scene(groupid, scene_id)
         sent += 3
         end = datetime.datetime.now()
         elapsed = end - start
         wait_for = tick - elapsed
         time.sleep(wait_for.total_seconds())
-
-
-def init_scenes(bridge):
-    bridge.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': False, 'transitiontime': 0})
-    bridge.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": False, 'transitiontime': 0})
-    bridge.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': False, 'transitiontime': 0})
-    bridge.request(mode='POST', address='/api/' + bridge.username + '/scenes/', data={'name': '0', 'lights': ['1', '2', '3'], 'recycle': True})
-    bridge.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': False, 'transitiontime': 0})
-    bridge.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": False, 'transitiontime': 0})
-    bridge.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
-    bridge.request(mode='POST', address='/api/' + bridge.username + '/scenes/', data={'name': '1', 'lights': ['1', '2', '3'], 'recycle': True})
-    bridge.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': False, 'transitiontime': 0})
-    bridge.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": True, 'transitiontime': 0})
-    bridge.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': False, 'transitiontime': 0})
-    bridge.request(mode='POST', address='/api/' + bridge.username + '/scenes/', data={'name': '2', 'lights': ['1', '2', '3'], 'recycle': True})
-    bridge.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': False, 'transitiontime': 0})
-    bridge.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": True, 'transitiontime': 0})
-    bridge.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
-    bridge.request(mode='POST', address='/api/' + bridge.username + '/scenes/', data={'name': '3', 'lights': ['1', '2', '3'], 'recycle': True})
-    bridge.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
-    bridge.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": False, 'transitiontime': 0})
-    bridge.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': False, 'transitiontime': 0})
-    bridge.request(mode='POST', address='/api/' + bridge.username + '/scenes/', data={'name': '4', 'lights': ['1', '2', '3'], 'recycle': True})
-    bridge.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
-    bridge.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": False, 'transitiontime': 0})
-    bridge.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
-    bridge.request(mode='POST', address='/api/' + bridge.username + '/scenes/', data={'name': '5', 'lights': ['1', '2', '3'], 'recycle': True})
-    bridge.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
-    bridge.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": True, 'transitiontime': 0})
-    bridge.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': False, 'transitiontime': 0})
-    bridge.request(mode='POST', address='/api/' + bridge.username + '/scenes/', data={'name': '6', 'lights': ['1', '2', '3'], 'recycle': True})
-    bridge.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
-    bridge.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": True, 'transitiontime': 0})
-    bridge.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
-    bridge.request(mode='POST', address='/api/' + bridge.username + '/scenes/', data={'name': '7', 'lights': ['1', '2', '3'], 'recycle': True})
 
 
 b = Bridge('10.1.228.193')
@@ -80,8 +80,8 @@ b.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': True, 'transitiontim
 b.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": True, 'transitiontime': 0})
 b.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
 time.sleep(1)
-if (not b.get_group(1, 'hueheffner')):
-    b.create_group('hueheffner', [1, 2, 3])
+
+b.set_group(groupid, 'lights', [1,2,3])
 
 # brightness = 250
 # while(1):
@@ -90,4 +90,4 @@ if (not b.get_group(1, 'hueheffner')):
 # time.sleep(0.5)
 
 say_nrz(b, [1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0], [1, 2, 3])
-init_scenes(b)
+#init_scenes(b)
