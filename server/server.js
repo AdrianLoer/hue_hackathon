22710d3dc20c984c8b39236eef1a7752b909e5bc
Adrian Loer <adrian.loer@gmail.com>
2017-09-30 21:45:16 +0200

    no message

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var bodyParser = require('body-parser')
 var http = require("http")
 var WebSocketServer = require("ws").Server
+var _ = require('lodash');
 
 var path = require('path');
 
@@ -10,11 +11,16 @@ const express = require('express');
 // Constants
 const PORT = 8080;
 
-// App
-const app = express();
-// server = http.createServer(app);
+var expressWs = require('express-ws')
 
-var expressWs = require('express-ws')(app);
+var expressWs = expressWs(express());
+
+var app = expressWs.app;
+// // App
+// const app = express();
+// // server = http.createServer(app);
+
+// var expressWs = require('express-ws')(app);
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -48,27 +54,42 @@ app.get("/getAllUsers", function(req, res) {
 
 app.post("/setColor", function(req, res) {
     console.log(req.body)
-        if (!req.body) return res.sendStatus(400)
-        if (exists(req.body)) {
-        	var value = req.body.value;
-        }
+    if (!req.body) return res.sendStatus(400)
+    if (exists(req.body)) {
+        var value = req.body.value;
+    }
 })
 
 app.ws('/ws', function(ws, req) {
-	console.log("echo called")
-  ws.on('message', function(msg) {
-  	var msgObj = {msg: msg}
-  	console.log(JSON.stringify(msgObj))
-    ws.send(msg);
-  });
+    console.log("echo called")
+    ws.on('message', function(msg) {
+        var msgObj = { msg: msg }
+        // console.log(JSON.stringify(msgObj))
+        // wss.clients.forEach(function(client) {
+        //     client.send(JSON.stringify(msgObj));
+        // });
+        throttledImage(msgObj)
+        // ws.send(msgObj);
+    });
 });
+
+var wss = expressWs.getWss('/ws');
 
 
 app.get("/map", function(req, res) {
- res.sendFile(path.join(__dirname,  "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 })
 
-
+var throttledImage = _.throttle(function(msgObj) {
+	console.log("test")
+	// var base64Data = msgObj.msg.replace(/^data:image\/png;base64,/, "");
+	require("fs").writeFile("/output/out" + (new Date().getTime()) + ".jpg", msgObj.msg, 'base64', function(err) {
+	  console.log(err);
+	});
+	wss.clients.forEach(function(client) {
+	    client.send(JSON.stringify(msgObj));
+	});
+}, 100)
 
 app.listen(PORT);
 // server.listen(PORT);
@@ -86,7 +107,7 @@ console.log('Running on ' + PORT);
 // wss.on('open', function open() {
 //   ws.send('something');
 // });
- 
+
 // wss.on('message', function incoming(data) {
 //   console.log(data);
 // });
