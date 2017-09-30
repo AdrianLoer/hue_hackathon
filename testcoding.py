f7b6efbfae044a28f5a5cd69fda358b3fe7be71a
tom <shizzl@thecout.com>
2017-09-30 17:53:02 +0200

    added basic loggin whatsoever

--- testcoding.py
+++ testcoding.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 from phue import Bridge
+import logging
+logging.basicConfig()
 import time
 
 b = Bridge('10.1.228.193')
@@ -11,13 +13,14 @@ b.connect()
 # Get the bridge state (This returns the full dictionary that you can explore)
 b.get_api()
 
-b.set_light(1,{"hue":25500, "sat":255, "bri":255})
-b.set_light(2,{"hue":46920, "sat":255, "bri":255})
-b.set_light(3,{"hue":65280, "sat":255, "bri":255})
+b.set_light(1,{'on': True,"hue":25500, "sat":255, "bri":255})
+b.set_light(2,{'on': True,"hue":46920, "sat":255, "bri":255})
+b.set_light(3,{'on': True,"hue":65280, "sat":255, "bri":255})
 
 
-brightness = 250
+brightness = 0
 while(1):
-    brightness = 255 if brightness == 250 else 250
-    b.set_light(1,{'transitiontime': 0, 'on': True, 'ct': brightness})
+    brightness = 255 if brightness == 0 else 0
+    b.set_light(1,{'transitiontime': 0, 'on': True, 'sat': brightness})
     time.sleep(0.5)
+
