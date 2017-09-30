b88d0ba8a8700aa3b7af93dedca5b4bb59a28199
tom <shizzl@thecout.com>
2017-09-30 18:58:42 +0200

    group added and scenery skeleton

--- testcoding.py
+++ testcoding.py
@@ -2,11 +2,14 @@
 
 from phue import Bridge
 import logging
+
 logging.basicConfig()
 import time
 import datetime
 
 tick = datetime.timedelta(milliseconds=500)
+
+scenes = {"000": 0, "001": 1, "010": 2, "011": 3, "100": 4, "101": 5, "110": 6, "111": 7}
 
 
 def say_nrz(bridge, data, lights):
@@ -18,15 +21,16 @@ def say_nrz(bridge, data, lights):
     length += stuffing
     while sent < length:
         start = datetime.datetime.now()
-        for i in range(0, len(lights)):
-            bridge.set_light(lights[i], {'on': not not data[sent], 'transitiontime': 0, 'bri': 255})
-            sent += 1
+
+        # for i in range(0, len(lights)):
+        #    bridge.set_light(lights[i], {'on': not not data[sent], 'transitiontime': 0, 'bri': 255})
+        scene_id = scenes[(''.join([str(x) for x in data[sent:sent + 3]]))]
+        bridge.run_scene("hueheffner", scene_id)
+        sent += 3
         end = datetime.datetime.now()
         elapsed = end - start
         wait_for = tick - elapsed
         time.sleep(wait_for.total_seconds())
-
-
 
 
 b = Bridge('10.1.228.193')
@@ -37,19 +41,17 @@ b.connect()
 # Get the bridge state (This returns the full dictionary that you can explore)
 b.get_api()
 
-
-b.set_light(1,{"hue":25500, "sat":255, "bri":255, 'on': True, 'transitiontime': 0})
-b.set_light(2,{"hue":46920, "sat":255, "bri":255, "on": True, 'transitiontime': 0})
-b.set_light(3,{"hue":65280, "sat":255, "bri":255, 'on': True, 'transitiontime': 0})
+b.set_light(1, {"hue": 25500, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
+b.set_light(2, {"hue": 46920, "sat": 255, "bri": 255, "on": True, 'transitiontime': 0})
+b.set_light(3, {"hue": 65280, "sat": 255, "bri": 255, 'on': True, 'transitiontime': 0})
 time.sleep(1)
+if (not b.get_group(1, 'lights')):
+    b.create_group('hueheffner', [1, 2, 3])
 
-#brightness = 250
-#while(1):
-    #brightness = 255 if brightness == 250 else 250
-    #b.set_light(1,{'transitiontime': 0, 'on': True, 'hue': brightness})
-    #time.sleep(0.5)
+# brightness = 250
+# while(1):
+# brightness = 255 if brightness == 250 else 250
+# b.set_light(1,{'transitiontime': 0, 'on': True, 'hue': brightness})
+# time.sleep(0.5)
 
 say_nrz(b, [1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0], [1, 2, 3])
-
-
-
